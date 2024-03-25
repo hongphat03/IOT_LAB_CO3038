19767bd112894fd49f207b2c96404c347a408f6a
Phat Nguyen <79517243+hongphat03@users.noreply.github.com>
2024-03-25 22:59:21 +0700

    LAB3

--- main.py
+++ main.py
@@ -1,50 +1,43 @@
-from Adafruit_IO import Client, Feed
-import time
-from uart import readSerial
-from simple_ai import image_detector
+import sys
+from Adafruit_IO import MQTTClient
+from uart import writeData
+
 AIO_FEED_ID = ["nutnhan1","nutnhan2"]
-HOST = "tcp://io.adafruit.com"
-AIO_USERNAME = "hongphat03"
-AIO_KEY = "aio_FbQw34wVRTlxiUktSkRzGGhIys27"
-counter_ai = 5
-ai_result = ""
-previous_result = ""
-# def connected(client):
-#     print("Ket noi thanh cong ...")
-#     for topic in AIO_FEED_ID:
-#         client.subscribe(topic)
+AIO_USERNAME = ""
+AIO_KEY = ""
 
-# def subscribe(client , userdata , mid , granted_qos):
-#     print("Subscribe thanh cong ...")
+def connected(client):
+    print("Ket noi thanh cong ...")
+    for topic in AIO_FEED_ID:
+        client.subscribe(topic)
 
-# def disconnected(client):
-#     print("Ngat ket noi ...")
-#     sys.exit (1)
+def subscribe(client , userdata , mid , granted_qos):
+    print("Subscribe thanh cong ...")
 
-# def message(client , feed_id , payload):
-#     print("Nhan du lieu: " + payload)
+def disconnected(client):
+    print("Ngat ket noi ...")
+    sys.exit (1)
 
-# client = MQTTClient(AIO_USERNAME , AIO_KEY)
-# # client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
-# client.on_connect = connected
-# client.on_disconnect = disconnected
-# client.on_message = message
-# client.on_subscribe = subscribe
-# client.connect()
-# client.loop_background()
-# counter = 10
-client = Client(AIO_USERNAME, AIO_KEY)
-# test = client.feeds("nutnhan1")
-client.send_data("nutnhan1", 21)
+def message(client , feed_id , payload):
+    print("Nhan du lieu: " + payload + "feed_id:" + feed_id)
+    if feed_id == "nutnhan1":
+        if payload == "0":
+            writeData("1")
+        else:
+            writeData("2")
+    if feed_id == "nutnhan2":
+        if payload == "0":
+            writeData("3")
+        else:
+            writeData("4")
+
+client = MQTTClient(AIO_USERNAME , AIO_KEY)
+client.on_connect = connected
+client.on_disconnect = disconnected
+client.on_message = message
+client.on_subscribe = subscribe
+client.connect()
+client.loop_background()
+
 while True:
-    counter_ai = counter_ai - 1
-    if counter_ai <= 0 :
-        # todo for AI
-        counter_ai = 5
-        previous_result = ai_result
-        ai_result = image_detector()
-        print("AI Output: ", ai_result)
-        if previous_result != ai_result:
-            client.send_data("ai",ai_result[2:])
-    # readSerial(client)
-    time.sleep(1)
+    pass
--- uart.py
+++ uart.py
@@ -41,3 +41,6 @@ def readSerial(client):
                 mess = ""
             else:
                 mess = mess[end+1:]
+
+def writeData(data):
+    ser.write((str(data)).encode())
